d55063c6e68d865fff8613bb52c4ce99cbdc5d2f
Sara-Albagir <saraaalbagiir@gmail.com>
2023-09-08 11:06:39 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - is he main f
+ * 
+ * Return: equals 0
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
