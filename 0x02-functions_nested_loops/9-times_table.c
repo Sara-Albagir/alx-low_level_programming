b9ef52aee8ab9a5b1712a520bdcb4745b0feace2
Sara-Albagir <saraaalbagiir@gmail.com>
2023-09-12 12:24:58 -0700

    task 10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,22 +8,33 @@
 
 void times_table(void)
 {
-	int i, j, result;
+	int x, y, z, u, d;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (x = 0 ; x <= 9 ; x++)
 	{
-		for (j = 0 ; j < 10 ; j++)
-		{
-			result = i * j;
-			if (j == 0)
-				printf("%d", result);
-			else
-			{
-				printf("%2d", result);
-				if (j != 9)
-					printf(" ,");
-			}
-		}
-		printf("\n");
+	for (y = 0 ; y <= 9 ; y++)
+	{
+	z = x * y;
+	if (z > 9)
+	{
+	u = z % 10;
+	d = (z - u) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(d + '0');
+	_putchar(u + '0');
+	}
+	else
+	{
+	if (y != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(z + '0');
+	}
+	}
+	_putchar('\n');
 	}
 }
