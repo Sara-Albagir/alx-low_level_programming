7ccb4d9fe94ac3049572e9462ac7407bed2d5edf
Sara-Albagir <saraaalbagiir@gmail.com>
2023-09-15 05:14:35 -0700

    task 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,7 @@ void print_numbers(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 9 ; i++)
-	{
-		_putchar(i + '0');
-	}
-	_putchar('\n';
+	for (i = 0 ; i <= 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
 }
