4e0f3d8d0c32ac99cdbf1953dbf7c35f395e1a5a
Sara-Albagir <saraaalbagiir@gmail.com>
2023-09-26 03:02:40 -0700

    t0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -10,4 +11,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 
-#endif /* MAIN_H
+#endif
