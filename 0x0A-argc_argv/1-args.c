baee367ed9bbe79d26c3eb71a7a5550202780ab5
Sara-Albagir <saraaalbagiir@gmail.com>
2023-10-02 15:18:48 -0700

    t1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - themain f
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
